75fd018b31c417b0a7223e1f3c1de16f3a97da60
Madeline Ng <madelineng@madelines-mbp-3.dynamic.ucsd.edu>
2019-02-21 10:54:17 -0800

    changes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
+
+exports.viewAlt = function(request, response){
+	response.render('index', projects);
+};
+
 exports.view = function(request, response){
   	response.render('index', projects);
 };
